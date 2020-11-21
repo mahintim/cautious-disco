1df84209fd18ffe986aa135fa9ca17acb3678d13
mahintim <48666028+mahintim@users.noreply.github.com>
2020-11-21 12:10:32 +0700

    Update 21.11.2020.py

--- 21.11.2020.py
+++ 21.11.2020.py
@@ -13,3 +13,11 @@ c = int(input())
 print(f'{a}+{b}+{c}={a + b + c}')
 print(f'{a}*{b}*{c}={a * b * c}')
 
+# C
+a = int(input('Введите три числа:\n'))
+b = int(input())
+c = int(input())
+
+print(f'{a}+{b}+{c}={a + b + c}')
+print(f'{a}*{b}*{c}={a * b * c}')
+print(f'({a}+{b}+{c})/3={(a + b + c) / 3}')
